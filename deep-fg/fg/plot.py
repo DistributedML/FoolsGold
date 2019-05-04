7813bbb198683185d17d7e74da13c3ba88b6d205
Chris Yoon <chris.jm.yoon@gmail.com>
2019-05-04 14:19:00 -0700

    Added code to plot gradients

--- deep-fg/fg/plot.py
+++ deep-fg/fg/plot.py
@@ -1,7 +1,23 @@
 import matplotlib.pyplot as plt
 import pdb
-
+import os
+import torch
 def plot_gradients(grads):
     plt.imshow(grads)
     plt.axes().set_aspect('auto')
     plt.clf()
+
+
+if __name__ == "__main__":
+    iid_dir = "../save/iidness/"
+    iid_file_path = os.path.join(iid_dir, "100-100.pth")
+    iid_exp = torch.load(iid_file_path)
+    memory = iid_exp['memory']
+    plt.imshow(memory, cmap=plt.cm.gray, vmin=0.0, vmax=10)
+    yticks = ["Client " + str(i+1) for i in range(10)]
+    yticks.extend(["Sybil " + str(i+1) for i in range(5)])
+    plt.yticks(ticks=range(15), labels=yticks)
+    plt.xlabel("Parameters")
+    plt.axes().set_aspect('auto')
+    plt.show()
+    pdb.set_trace()
